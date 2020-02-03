f009e73b1f2a33bd91cd1e2f7f413afc905100dd
Marcus Wordlaw <48574414+MarcusWordlaw@users.noreply.github.com>
2020-02-03 11:15:34 -0500

    Update seeds.rb

--- content-auditor-backend/db/seeds.rb
+++ content-auditor-backend/db/seeds.rb
@@ -3,7 +3,7 @@ User.destroy_all()
 Organization.destroy_all()
 
 
-IOMAXIS = Organization.create(name: "Consensys")
-TECHCANAL = Organization.create(name: "PegaSys")
-INGRESSIVE = Organization.create(name: "Kaleido")
-User.create!(name: 'Consensys Adminstrator', email: 'admin@consensys.com', password_digest: User.digest('1234'), address: "0x4", admin: true, organization_id: IOMAXIS.id )
+Consensys = Organization.create(name: "Consensys")
+Pegasys = Organization.create(name: "PegaSys")
+Kaleido = Organization.create(name: "Kaleido")
+User.create!(name: 'Consensys Adminstrator', email: 'admin@consensys.com', password_digest: User.digest('1234'), address: "0x4", admin: true, organization_id: Consensys.id )
